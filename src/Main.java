178f23d31e4a93691a57da2aeff3e6c356d72297
georgerappel <george.concei@hotmail.com>
2017-11-12 14:28:41 -0200

    Use ConcurrentHashMap

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,57 @@
+import javafx.util.Pair;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.Key;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Main {
 
-//    private static ConcurrentMap<Integer, Integer> mapaAssentos = new ConcurrentHashMap<>();
+    public static ConcurrentMap<Integer, Integer> t_Assentos = new ConcurrentHashMap<>();
+    public static Pair<Integer, Integer> t_Assento;
 
-    public static int t_Assentos[];
-    public static int t_Assento;
     public static String nome_arquivo = "teste.txt";
-    public static Estrutura x;
 
     public static void main(String args[]){
 
-//        mapaAssentos.put(1, 0);
-//        mapaAssentos.replace()
+        nome_arquivo = args[0];
 
         int qtd = Integer.parseInt(args[1]);
 
-        x = new Estrutura(qtd);
+        // Inicialização dos assentos com 0 - Livre
+        // Numeracao do assento 1 até o assento = qtd
+        for(int i = 1; i <= qtd; i++){
+            t_Assentos.put(i, 0);
+        }
 
-        Thread1 td1 = new Thread1(x);
+        Thread1 td1 = new Thread1();
 
         td1.setName("1");
         td1.start();
     }
 
-    public static class Estrutura{
-        static int qtd;
-        int i = 0;
-
-        Estrutura(int qtd){
-            this.qtd = qtd;
-            t_Assentos = new int[qtd];
-            for (int i = 0; i < qtd; i++ ){
-                t_Assentos[i] = 0;
-            }
-        }
-    }
-
     public static class Thread1 extends Thread{
-        private Estrutura x;
-
-        Thread1(Estrutura x){
-            this.x = x;
-        }
-
         public void run(){
             visualizaAssentos();
-            alocaAssentoDado(2,1);
+            alocaAssentoDado(2, 1);
             visualizaAssentos();
            // liberaAssento();
         }
     }
 
     public static class Thread2 extends Thread{
-        private Estrutura x;
-
-        Thread2(Estrutura x) { this.x = x;}
-
         public void run(){
             visualizaAssentos();
         }
     }
 
     public static void visualizaAssentos() {
-        for(int i = 0; i < Estrutura.qtd; i++){
-            System.out.println("Assento "+i+" com valor: "+t_Assentos[i]);
-        }
+        String listaAssentos = fazString();
+
+        System.out.println(listaAssentos);
 
         int id_thread = Integer.parseInt(Thread.currentThread().getName());
         String teste = fazString();
@@ -79,12 +60,12 @@ public class Main {
 
     public static int alocaAssentoDado(int assento, int id){
         if(id == Integer.parseInt(Thread.currentThread().getName())) {
-            if (t_Assentos[assento] == 0) {
-                t_Assentos[assento] = 1;
+            boolean reservado = t_Assentos.replace(assento, 0, id);
+            if ( reservado ) {
                 System.out.println("Assento reservado!");
                 int id_thread = Integer.parseInt(Thread.currentThread().getName());
-                String teste = fazString();
-                buffer(3,id_thread, assento, teste);
+                String listaAssentos = fazString();
+                buffer(3, id_thread, assento, listaAssentos);
                 return 1;
             } else {
                 System.out.println("Assento não reservado!");
@@ -98,7 +79,7 @@ public class Main {
 
     public static int alocaAssentoLivre(int id){
         return 0;
-    }
+    }cd tr
 
     public static int liberaAssento(int assento, int id){
         return 0;
@@ -107,12 +88,12 @@ public class Main {
     public static String fazString(){
         StringBuilder sb = new StringBuilder();
         sb.append("[");
-        for(int i = 0; i < Estrutura.qtd; i++){
-            if(i == Estrutura.qtd - 1){
-                sb.append(t_Assentos[i]);
+        for(int i = 1; i <= t_Assentos.size(); i++){
+            if(i == t_Assentos.size()){
+                sb.append(t_Assentos.get(i));
             }else {
-                sb.append(t_Assentos[i]);
-                sb.append(", ");
+                sb.append(t_Assentos.get(i));
+                sb.append(",");
             }
         }
         sb.append("]");
@@ -124,13 +105,13 @@ public class Main {
 
         if(assento == 0){
             try(BufferedWriter bw = new BufferedWriter((new FileWriter(nome_arquivo, true)))){
-                String content = codigo+", "+id_thread+", "+assentos;
+                String content = codigo+","+id_thread+","+assentos;
                 bw.newLine();
                 bw.write(content);
             } catch (IOException e){e.printStackTrace();}
         } else {
             try (BufferedWriter bw = new BufferedWriter((new FileWriter(nome_arquivo, true)))) {
-                String content = codigo + ", " + id_thread + ", " + assento + ", " + assentos;
+                String content = codigo + "," + id_thread + "," + assento + "," + assentos;
                 bw.newLine();
                 bw.write(content);
             } catch (IOException e) {
